5e796b06131934b3ae73ff37d00994e91a19474c
terens <37905314+TerensTare@users.noreply.github.com>
2021-02-26 20:56:52 +0100

    Update portfile.cmake

--- ports/boost-ut/portfile.cmake
+++ ports/boost-ut/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO boost-ext/ut
+    REPO TerensTare/ut
     REF 0b6812b8b37951b6af178c8f445519796b33fe23
     SHA512 8bdee1fff5e224561d9a03004bd53042c3536bb6f507264c802b49c2fd0820093d2877bd4bb05dec453edb8c0ee14ab1883d8a1cbe4c479d58bde36ab623074c
     HEAD_REF master
